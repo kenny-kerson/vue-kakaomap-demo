723c1b8a9161fe79e3c3d53a1f6d94b8983addd1
kenny-kerson <kenny.kerson@gmail.com>
2020-03-11 22:56:17 +0900

    소스 리팩토링분 반영

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -24,63 +24,21 @@
             }
         },
         mounted() {
-            // loadScriptOnce(`//dapi.kakao.com/v2/maps/sdk.js?appkey=7c8b78578a123cad363d0029fc147776`)
+            // loadScriptOnce(`//dapi.kakao.com/v2/maps/sdk.js?appkey=7c8b78578a123cad363d0029fc147776&libraries=services`)
             // .then(() => {
             //     this.openKakakoMap()
             // })
-            // loadScriptOnce(`//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=7c8b78578a123cad363d0029fc147776`)
-            //     .then(() => {
-            //         kakao.maps.load(() => {
-            //             this.render();
-            //             this.bindEvents();
-            //             this.$emit('load', this.map);
-            //         });
-            //     })
-            //     .catch(err => {
-            //         console.error(err);
-            //     });
             this.openKakakoMap()
         },
         methods: {
             openKakakoMap() {
-                // console.log( "__KENNY__ openKakakoMap()")
-                // var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-                // var options = { //지도를 생성할 때 필요한 기본 옵션
-                //     center: new kakao.maps.LatLng(37.56682, 126.97865),
-                //     level: 3,
-                //     mapTypeId : kakao.maps.MapTypeId.ROADMAP,
-                // };
-                //
-                // var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-                //
-                // // 지도에 확대 축소 컨트롤을 생성한다
-                // var zoomControl = new kakao.maps.ZoomControl();
-                //
-                // // 지도의 우측에 확대 축소 컨트롤을 추가한다
-                // map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-                //
-                // var marker = new kakao.maps.Marker({
-                //     position: new kakao.maps.LatLng(37.56682, 126.97865), // 마커의 좌표
-                //     map: map // 마커를 표시할 지도 객체
-                // });
-                //
-                // // 지도에 원을 표시한다
-                // var circle = new kakao.maps.Circle({
-                //     map: map, // 원을 표시할 지도 객체
-                //     center : new kakao.maps.LatLng(37.56782, 126.97665), // 지도의 중심 좌표
-                //     radius : 50, // 원의 반지름 (단위 : m)
-                //     fillColor: '#FF0000', // 채움 색
-                //     fillOpacity: 0.5, // 채움 불투명도
-                //     strokeWeight: 3, // 선의 두께
-                //     strokeColor: '#FF0000', // 선 색
-                //     strokeOpacity: 0.9, // 선 투명도
-                //     strokeStyle: 'solid' // 선 스타일
-                // });
+                console.log( "__KENNY__ openKakakoMap()")
 
                 // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
                 this.infowindow = new kakao.maps.InfoWindow({zIndex: 1});
 
-                this.mapContainer = document.getElementById('map'), // 지도를 표시할 div
+                // 지도를 표시할 div
+                this.mapContainer = document.getElementById('map')
                 this.mapOption = {
                         center: new kakao.maps.LatLng(37.401470, 127.108642), // 지도의 중심좌표
                         level: 3 // 지도의 확대 레벨
@@ -110,7 +68,7 @@
                     // LatLngBounds 객체에 좌표를 추가합니다
                     this.bounds = new kakao.maps.LatLngBounds();
 
-                    for (var i = 0; i < data.length; i++) {
+                    for (let i = 0; i < data.length; i++) {
                         console.log( '__KENNY__ data[i] : ' )
                         console.log( data[i] )
 
@@ -127,7 +85,7 @@
                 console.log( data )
 
                 // 마커를 생성하고 지도에 표시합니다
-                var marker1 = new kakao.maps.Marker({
+                let marker1 = new kakao.maps.Marker({
                     map: this.map,
                     position: new kakao.maps.LatLng(data.y, data.x)
                 });
